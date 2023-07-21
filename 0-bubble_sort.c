872b86e0270ebeac6c4dab266298ca157a2a092f
Samuel <samueleibo12@gmail.com>
2023-07-21 03:26:57 -0700

    add new file

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,18 +10,23 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	int temp, i, j;
+	int temp;
+	size_t i, j, k;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < n - i - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
 				temp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
-			}
+
+				for (k = 0; k < size; k++)
+					printf("%d ", array[k]);
+				printf("\n");
+			}	
 		}
 	}
 }
