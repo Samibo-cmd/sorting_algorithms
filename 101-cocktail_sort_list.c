6cc1e1a08b18b7f290f304145c947d7fa00647bd
Valentine1244 <akamigboifeanyi22@gmail.com>
2023-07-25 07:01:22 -0700

    Cocktail

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -7,7 +7,7 @@ void cocktail_sort_list(listint_t **list);
 /**
  * swap_node_ahead - Swap a node in a listint_t doubly-linked list
  *
- * Ifeanyi   list of integers with the node ahead of it.
+ * Martin   list of integers with the node ahead of it.
  * @list: A pointer to the head of a doubly-linked list of integers.
  * @tail: A pointer to the tail of the doubly-linked list.
  * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
@@ -34,7 +34,7 @@ void swap_node_ahead(listint_t **list, listint_t **tail, listint_t **shaker)
 /**
  * swap_node_behind - Swap a node in a listint_t doubly-linked
  *
- * Valentine    list of integers with the node behind it.
+ * Akugu    list of integers with the node behind it.
  * @list: A pointer to the head of a doubly-linked list of integers.
  * @tail: A pointer to the tail of the doubly-linked list.
  * @shaker: A pointer to the current swapping node of the cocktail shaker algo.
@@ -61,7 +61,7 @@ void swap_node_behind(listint_t **list, listint_t **tail, listint_t **shaker)
 /**
  * cocktail_sort_list - Sort a listint_t doubly-linked list of integers in
  *
- * Ifeanyi    ascending order using the cocktail shaker algorithm.
+ * Martin    ascending order using the cocktail shaker algorithm.
  * @list: A pointer to the head of a listint_t doubly-linked list.
  */
 void cocktail_sort_list(listint_t **list)
