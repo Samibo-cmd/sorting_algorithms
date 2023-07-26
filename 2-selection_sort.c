29fe470bf0c8637dc992f6eeba6b75822e389fe1
Samuel <samueleibo12@gmail.com>
2023-07-26 03:15:33 -0700

    modify file "2-selection_sort.c"

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -13,6 +13,9 @@ void selection_sort(int *array, size_t size)
 	int temp;
 	size_t i, j, min_idx;
 
+	if (array == NULL || size < 2)
+		return;
+
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
